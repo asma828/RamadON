c665974ca129050c03f329abf40a9f3f43f39a43
asma828 <asmalachhab66@gmail.com>
2025-02-18 12:09:29 +0100

    create the table in the basedonnee

--- RAMADON/database/migrations/2025_02_18_105348_create_experiences_table.php
+++ RAMADON/database/migrations/2025_02_18_105348_create_experiences_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('experiences', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique()->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('like_count')->default(0);
             $table->timestamps();
         });
     }
@@ -24,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('experiences');
     }
 };
