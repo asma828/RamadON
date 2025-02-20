6cec5d00e7f409af4dea87387992c023b2b68e42
asma828 <asmalachhab66@gmail.com>
2025-02-20 14:04:44 +0100

    add functionnalité for adding Comment

--- RAMADON/app/Http/Controllers/CommentController.php
+++ RAMADON/app/Http/Controllers/CommentController.php
@@ -1,10 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Comment;
+use App\Models\Experience;
+use App\Models\Recipe;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function getExperienceComments(Experience $experience)
+    {
+        $experience->load('user');
+        $comments = $experience->comments()->with('user')->latest()->get();
+        
+        return response()->json([
+            'experience' => $experience,
+            'comments' => $comments
+        ]);
+    }
+
+    public function storeExperienceComment(Request $request, Experience $experience)
+    {
+        $validated = $request->validate([
+            'content' => 'required|max:1000',
+        ]);
+
+        $comment = new Comment();
+        $comment->user_id = Auth::id() ?? 1;
+        $comment->experience_id = $experience->id;
+        $comment->content = $validated['content'];
+        $comment->save();
+
+        $comment->load('user');
+        return response()->json($comment);
+    }
 }
