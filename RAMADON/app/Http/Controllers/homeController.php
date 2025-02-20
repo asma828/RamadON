7b4bba1599d3ee5485cc0349ca8e819d1688e813
asma828 <asmalachhab66@gmail.com>
2025-02-20 14:01:23 +0100

    display the experience and the the recipe with statistique

--- RAMADON/app/Http/Controllers/homeController.php
+++ RAMADON/app/Http/Controllers/homeController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Experience;
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
-    //
+    public function index()
+    {
+        $experiences = Experience::with('user')
+            ->latest()
+            ->take(2)
+            ->get();
+
+  $popularRecipes = Recipe::with(['user', 'category'])
+  ->take(3)
+  ->get();
+
+ $totalPosts = Experience::count();
+ $totalRecipes = Recipe::count();
+
+ return view('home', compact('experiences', 'popularRecipes', 'totalPosts', 'totalRecipes'));
+}
 }
