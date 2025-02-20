114160fae446ef98186adfb28d4841213cbd9543
asma828 <asmalachhab66@gmail.com>
2025-02-20 14:08:28 +0100

    add the necessairy router

--- RAMADON/routes/web.php
+++ RAMADON/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ExperienceController;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\CommentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route::get('/salam/{nom}/{prenom}',function(Request $Request){
+//  return view('salam',[
+//     'nom'=>$Request->nom,
+//     'prenom'=>$Request->prenom
+//  ]);
+
+// });
+
+// Experiences
+Route::get('/experiences', [ExperienceController::class, 'index'])->name('experiences.index');
+Route::post('/experiences', [ExperienceController::class, 'store'])->name('experiences.store');
+
+// Recipes
+Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index');
+Route::post('/recipes', [RecipeController::class, 'store'])->name('recipes.store');
+
+// Comments
+Route::get('/experiences/{experience}/comments', [CommentController::class, 'getExperienceComments']);
+Route::post('/experiences/{experience}/comments', [CommentController::class, 'storeExperienceComment'])
+    ->name('experiences.comments.store');
+
+    Route::get('/experiences/{experience}/comments', [CommentController::class, 'getExperienceComments']);
