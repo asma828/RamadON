832aa7e692287e3ccb101249c6a79d3757130e9b
asma828 <asmalachhab66@gmail.com>
2025-02-20 14:17:33 +0100

    add the comment modal blade view

---
+++ RAMADON/resources/views/partials/comments-modal.blade.php
@@ -0,0 +1,13 @@
+<div id="commentsModal" class="modal-backdrop" style="display: none;">
+    <div class="modal">
+        <div class="border-b p-4 flex justify-between items-center">
+            <h3 class="text-xl font-semibold">Commentaires</h3>
+            <button onclick="closeCommentsModal()" class="text-gray-500 hover:text-gray-700">
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+        <div class="modal-body p-4">
+            <!-- Le contenu sera chargé dynamiquement -->
+        </div>
+    </div>
+</div>
